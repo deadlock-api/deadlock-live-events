f87658ca8b2ad6e4ac1c5e66a19d2dbaf451c884
Manuel Raimann <raimannma@outlook.de>
2025-08-19 18:02:36 +0200

    Handle parsing errors in demo stream by breaking the loop on error

--- src/events.rs
+++ src/events.rs
@@ -82,6 +82,7 @@ async fn demo_event_stream(
                 Some(Ok(_)) => {
                     if let Err(e) = parser.run_to_end().await {
                         error!("Error while parsing demo stream: {e}");
+                        break;
                     }
                 }
                 Some(Err(err)) => {
