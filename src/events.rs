3989ca82395ff108b0c22c2bae8eb4e9a6f2cffc
Manuel Raimann <raimannma@outlook.de>
2025-08-19 08:26:56 +0200

    Remove early return on parsing error in demo stream

--- src/events.rs
+++ src/events.rs
@@ -82,7 +82,6 @@ async fn demo_event_stream(
                 Some(Ok(_)) => {
                     if let Err(e) = parser.run_to_end().await {
                         error!("Error while parsing demo stream: {e}");
-                        return;
                     }
                 }
                 Some(Err(err)) => {
