49a22c394d662a3182b6d01489940c73f8e021f5
Manuel Raimann <raimannma@outlook.de>
2026-01-02 00:00:38 +0100

    Refactor hash constants to use fkey_from_path for player data fields

--- src/demo_parser/hashes.rs
+++ src/demo_parser/hashes.rs
@@ -8,7 +8,8 @@ pub(super) const STEAM_ID_HASH: u64 = fxhash::hash_bytes(b"m_steamID");
 pub(super) const STEAM_NAME_HASH: u64 = fxhash::hash_bytes(b"m_iszPlayerName");
 pub(super) const HERO_BUILD_ID_HASH: u64 = fxhash::hash_bytes(b"m_unHeroBuildID");
 pub(super) const HERO_BUILD_SERIALIZED_HASH: u64 = fxhash::hash_bytes(b"m_sHeroBuildSerialized");
-pub(super) const HERO_BADGE_XP_HASH: u64 = fxhash::hash_bytes(b"m_unHeroBadgeXP");
+pub(super) const HERO_BADGE_XP_HASH: u64 =
+    fkey_from_path(&["m_PlayerDataGlobal", "m_unHeroBadgeXP"]);
 pub(super) const LEVEL_HASH: u64 = fxhash::hash_bytes(b"m_nLevel");
 pub(super) const TEAM_HASH: u64 = fxhash::hash_bytes(b"m_iTeamNum");
 pub(super) const SCORE_HASH: u64 = fxhash::hash_bytes(b"m_iScore");
@@ -16,26 +17,29 @@ pub(super) const TEAMNAME_HASH: u64 = fxhash::hash_bytes(b"m_szTeamname");
 pub(super) const FLEX_UNLOCKED_HASH: u64 = fxhash::hash_bytes(b"m_nFlexSlotsUnlocked");
 pub(super) const HEALTH_HASH: u64 = fxhash::hash_bytes(b"m_iHealth");
 pub(super) const MAX_HEALTH_HASH: u64 = fxhash::hash_bytes(b"m_iMaxHealth");
-pub(super) const HERO_ID_HASH: u64 = fxhash::hash_bytes(b"m_nHeroID");
+pub(super) const HERO_ID_HASH: u64 = fkey_from_path(&["m_PlayerDataGlobal", "m_nHeroID"]);
 pub(super) const PLAYER_SLOT_HASH: u64 = fxhash::hash_bytes(b"m_unLobbyPlayerSlot");
 pub(super) const RANK_HASH: u64 = fxhash::hash_bytes(b"m_nCurrentRank");
 pub(super) const ASSIGNED_LANE_HASH: u64 = fxhash::hash_bytes(b"m_nAssignedLane");
 pub(super) const ORIGINAL_ASSIGNED_LANE_HASH: u64 =
     fxhash::hash_bytes(b"m_nOriginalLaneAssignment");
-pub(super) const NET_WORTH_HASH: u64 = fxhash::hash_bytes(b"m_iGoldNetWorth");
+pub(super) const NET_WORTH_HASH: u64 = fkey_from_path(&["m_PlayerDataGlobal", "m_iGoldNetWorth"]);
 pub(super) const HEALTH_REGEN_HASH: u64 = fxhash::hash_bytes(b"m_flHealthRegen");
-pub(super) const ULTIMATE_TRAINED_HASH: u64 = fxhash::hash_bytes(b"m_bUltimateTrained");
-pub(super) const KILLS_HASH: u64 = fxhash::hash_bytes(b"m_iPlayerKills");
-pub(super) const ASSISTS_HASH: u64 = fxhash::hash_bytes(b"m_iPlayerAssists");
-pub(super) const DEATHS_HASH: u64 = fxhash::hash_bytes(b"m_iDeaths");
-pub(super) const DENIES_HASH: u64 = fxhash::hash_bytes(b"m_iDenies");
-pub(super) const LAST_HITS_HASH: u64 = fxhash::hash_bytes(b"m_iLastHits");
-pub(super) const HERO_HEALING_HASH: u64 = fxhash::hash_bytes(b"m_iHeroHealing");
-pub(super) const SELF_HEALING_HASH: u64 = fxhash::hash_bytes(b"m_iSelfHealing");
-pub(super) const HERO_DAMAGE_HASH: u64 = fxhash::hash_bytes(b"m_iHeroDamage");
-pub(super) const OBJECTIVE_DAMAGE_HASH: u64 = fxhash::hash_bytes(b"m_iObjectiveDamage");
-pub(super) const ULTIMATE_COOLDOWN_END_HASH: u64 = fxhash::hash_bytes(b"m_flUltimateCooldownEnd");
-pub(super) const UPGRADES_HASH: u64 = fxhash::hash_bytes(b"m_vecUpgrades");
+pub(super) const ULTIMATE_TRAINED_HASH: u64 =
+    fkey_from_path(&["m_PlayerDataGlobal", "m_bUltimateTrained"]);
+pub(super) const KILLS_HASH: u64 = fkey_from_path(&["m_PlayerDataGlobal", "m_iPlayerKills"]);
+pub(super) const ASSISTS_HASH: u64 = fkey_from_path(&["m_PlayerDataGlobal", "m_iPlayerAssists"]);
+pub(super) const DEATHS_HASH: u64 = fkey_from_path(&["m_PlayerDataGlobal", "m_iDeaths"]);
+pub(super) const DENIES_HASH: u64 = fkey_from_path(&["m_PlayerDataGlobal", "m_iDenies"]);
+pub(super) const LAST_HITS_HASH: u64 = fkey_from_path(&["m_PlayerDataGlobal", "m_iLastHits"]);
+pub(super) const HERO_HEALING_HASH: u64 = fkey_from_path(&["m_PlayerDataGlobal", "m_iHeroHealing"]);
+pub(super) const SELF_HEALING_HASH: u64 = fkey_from_path(&["m_PlayerDataGlobal", "m_iSelfHealing"]);
+pub(super) const HERO_DAMAGE_HASH: u64 = fkey_from_path(&["m_PlayerDataGlobal", "m_iHeroDamage"]);
+pub(super) const OBJECTIVE_DAMAGE_HASH: u64 =
+    fkey_from_path(&["m_PlayerDataGlobal", "m_iObjectiveDamage"]);
+pub(super) const ULTIMATE_COOLDOWN_END_HASH: u64 =
+    fkey_from_path(&["m_PlayerDataGlobal", "m_flUltimateCooldownEnd"]);
+pub(super) const UPGRADES_HASH: u64 = fkey_from_path(&["m_PlayerDataGlobal", "m_vecUpgrades"]);
 pub(super) const QUICKBUY_AUTO_PURCHASE_HASH: u64 = fxhash::hash_bytes(b"m_bQuickbuyAutoPurchase");
 pub(super) const QUICKBUY_AUTO_QUUE_BUILD_HASH: u64 =
     fxhash::hash_bytes(b"m_bQuickbuyAutoQueueBuild");
